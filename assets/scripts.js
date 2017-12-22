097d2c04a20d9098d048ac586d05acf6529a6955
intothewhile <intothewhile78@gmail.com>
2017-12-22 08:43:23 +0100

    avancement menu

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,16 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  var menuaction = document.querySelectorAll('.action-menu');
+            var pageheader = document.querySelector('.page-header');
+
+
+            [].slice.call(menuaction).forEach(function(el,i){
+            el.addEventListener('click', function(e) {
+              e.preventDefault();
+              pageheader.classList.toggle('menu-opened');
+              document.body.classList.toggle('overflowh');
+
+            });
+            });
+
+  //$('[data-toggle="tooltip"]').tooltip()
 })
