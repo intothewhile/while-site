177ad2cb04aafe8f44ba92470688a3434eb585c8
intothewhile <intothewhile78@gmail.com>
2018-01-15 11:41:17 +0100

    simplification navigation

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,21 @@
-$(function () {
-  var menuaction = document.querySelectorAll('.action-menu');
+(function(document) {
+        var toggle = document.querySelector('.sidebar-toggle');
+        var sidebar = document.querySelector('#sidebar');
+        var checkbox = document.querySelector('#sidebar-checkbox');
+
+        document.addEventListener('click', function(e) {
+          var target = e.target;
+
+          if(!checkbox.checked ||
+             sidebar.contains(target) ||
+             (target === checkbox || target === toggle)) return;
+
+          checkbox.checked = false;
+        }, false);
+      })(document);
+
+      $(function () {
+  /*var menuaction = document.querySelectorAll('.action-menu');
             var pageheader = document.querySelector('.page-header');
 
 
@@ -10,7 +26,7 @@ $(function () {
               document.body.classList.toggle('overflowh');
 
             });
-            });
+          });*/
 
   //$('[data-toggle="tooltip"]').tooltip()
 })
