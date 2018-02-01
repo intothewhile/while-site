d866e64251119470434512bb5966a907e117c9a7
intothewhile <intothewhile78@gmail.com>
2018-02-01 08:33:55 +0100

    modif header sur tous les types de page

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,23 +12,5 @@
 
           checkbox.checked = false;
         }, false);
-        
+
       })(document);
-
-
-      $(function () {
-  /*var menuaction = document.querySelectorAll('.action-menu');
-            var pageheader = document.querySelector('.page-header');
-
-
-            [].slice.call(menuaction).forEach(function(el,i){
-            el.addEventListener('click', function(e) {
-              e.preventDefault();
-              pageheader.classList.toggle('menu-opened');
-              document.body.classList.toggle('overflowh');
-
-            });
-          });*/
-
-  //$('[data-toggle="tooltip"]').tooltip()
-})
