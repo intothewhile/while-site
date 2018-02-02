700585ab6ca91db7c999688c502ca245c21f0f45
intothewhile <intothewhile78@gmail.com>
2018-02-02 10:41:05 +0100

    sticky header

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,18 @@
+
+
 (function(document) {
         var toggle = document.querySelector('.sidebar-toggle');
         var sidebar = document.querySelector('#sidebar');
         var checkbox = document.querySelector('#sidebar-checkbox');
+
+        /* sticky header */
+        window.addEventListener('scroll', function() {
+          if( window.pageYOffset > 200 ) {
+            document.querySelector('#mainNav').classList.add('is-visible');
+          } else {
+            document.querySelector('#mainNav').classList.remove('is-visible');
+          }
+        });
 
         document.addEventListener('click', function(e) {
           var target = e.target;
